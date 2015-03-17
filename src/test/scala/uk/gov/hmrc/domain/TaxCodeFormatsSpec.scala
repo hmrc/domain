73707b7d843c1fe82e4e7fb9889de4f64ccd6f6a
Charles Kubicek <charles@kubitech.com>
2015-03-17 15:08:57 +0000

    Header generated by v1.2.0 of sbt-header plugin

--- src/test/scala/uk/gov/hmrc/domain/TaxCodeFormatsSpec.scala
+++ src/test/scala/uk/gov/hmrc/domain/TaxCodeFormatsSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 HMRC
+ * Copyright 2015 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
