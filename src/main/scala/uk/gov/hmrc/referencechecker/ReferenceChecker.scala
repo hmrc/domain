744192df5ae7192f8bd3831aa7b229a625c01c6f
andy-gray <straightjacket_ltd@hotmail.com>
2016-04-22 12:06:13 +0100

    add reference checker from verbatim reference-checker project

---
+++ src/main/scala/uk/gov/hmrc/referencechecker/ReferenceChecker.scala
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.referencechecker
+
+import scala.util.Try
+
+trait ReferenceChecker {
+
+  val letterToNumber: Map[Char, Int]
+  val weights: List[Int]
+
+  val refRegex: String
+  def extraChecks: List[String=>Boolean] = List(ref => ref.matches(refRegex))
+  def mainCheck(reference:String, weightedSum:Int): Boolean
+
+  def referenceToValidate(reference:String): String = reference
+  def prepareReference(reference:String): String = reference
+
+  implicit class StringImprovements(s: String) {
+    def dropCharAtIndex(n: Int) = {
+      val (l1, l2) = s.toList splitAt n
+      val charList = l1 ::: (l2 drop 1)
+      charList.mkString
+    }
+  }
+
+  implicit class CharImprovements(digitOrLetter: Char) {
+    def toNumber: Int = letterToNumber.getOrElse(digitOrLetter, digitOrLetter.asDigit)
+  }
+
+  def isValid(reference: String): Boolean = {
+    val preparedReference = prepareReference(reference.toUpperCase)
+
+    val mainCheckResult = Try {
+
+      val refToValidate = referenceToValidate(preparedReference)
+
+      val weightedSum = weights.zipWithIndex.collect {
+                          case (weight, index) if index < refToValidate.length => {
+                            weight * refToValidate(index).toNumber
+                          }
+                        }.sum
+
+      mainCheck(preparedReference, weightedSum)
+
+    } getOrElse false
+
+    extraChecks.foldLeft(mainCheckResult) { (fullCondition, extraCheck) =>
+      fullCondition && extraCheck(preparedReference.toUpperCase)
+    }
+
+  }
+}
+
+trait ModulusReferenceChecker extends ReferenceChecker {
+
+  val remainderLookupTable: List[Char]
+  val modulus: Int
+  val checkCharIndex: Int
+
+  def mainCheck(reference:String, weightedSum:Int) = {
+    remainderLookupTable(weightedSum % modulus) == reference(checkCharIndex)
+  }
+}
+
+trait DifferenceReferenceChecker extends ReferenceChecker {
+
+  def mainCheck(reference:String, weightedSum:Int) = {
+    def checkVatReference(sum: Int, expected: Int): Boolean = {
+      var sumLessThan97 = sum
+      while (sumLessThan97 > 97) {
+        sumLessThan97 -= 97
+      }
+      val calculatedDigits = 97 - sumLessThan97
+      calculatedDigits == expected
+    }
+
+    val expected = reference.takeRight(2).toInt
+    checkVatReference(weightedSum, expected) || checkVatReference(weightedSum + 55, expected)
+  }
+}
+
+trait LetterRemainderLookup extends ModulusReferenceChecker {
+  val remainderLookupTable = List('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'X', 'J', 'K', 'L', 'M', 'N', 'Y', 'P', 'Q', 'R', 'S', 'T', 'Z', 'V', 'W')
+  val modulus = 23
+}
+
+
+trait UtrReferenceChecker extends ModulusReferenceChecker {
+  val remainderLookupTable = List('2', '1', '9', '8', '7', '6', '5', '4', '3', '2', '1')
+  val modulus = 11
+  val checkCharIndex = 0
+
+  val weights = List(6, 7, 8, 9, 10, 5, 4, 3, 2)
+  val letterToNumber = Map.empty[Char,Int]
+  val refRegex = """^\d{10}$"""
+
+  override def referenceToValidate(reference:String) = reference.tail
+}
+
+object SelfAssessmentReferenceChecker extends UtrReferenceChecker {
+  override def prepareReference(reference: String) = if (reference.startsWith("K")) reference.drop(1)
+                                                     else if (reference.endsWith("K")) reference.dropRight(1)
+                                                     else reference
+}
+
+object CorporationTaxReferenceChecker extends UtrReferenceChecker
+
+object VatReferenceChecker extends DifferenceReferenceChecker {
+  val weights = List(8, 7, 6, 5, 4, 3, 2)
+  val letterToNumber = Map.empty[Char,Int]
+  val refRegex = """^\d{9}$"""
+}
+
+object EpayeReferenceChecker extends LetterRemainderLookup {
+  val checkCharIndex = 4
+  val weights = List(9, 10, 11, 12, 0, 8, 7, 6, 5, 4, 3, 2, 1)
+  val letterToNumber = Map('A' -> 33, 'B' -> 34, 'C' -> 35, 'D' -> 36, 'E' -> 37, 'F' -> 38, 'G' -> 39, 'H' -> 40, 'J' -> 42, 'K' -> 43, 'L' -> 44, 'M' -> 45,
+    'N' -> 46, 'P' -> 48, 'Q' -> 49, 'R' -> 50, 'S' -> 51, 'T' -> 52, 'U' -> 56, 'V' -> 54, 'W' -> 55, 'X' -> 41, 'Y' -> 47, 'Z' -> 53)
+  val refRegex= """^\d{3}P[A-Z]\d{6}[A-Z_0-9]{2}$"""
+  override def extraChecks = super.extraChecks :+
+    ((ref:String) => if (ref.startsWith("961") && ref(12) == 'X') ref(5) == '0' else true) :+
+    ((ref:String) => if (ref.startsWith("961") && ref(12) != 'X') ref.matches( """^961P[A-Z]\d{8}$""") else true)
+}
+
+object OtherTaxReferenceChecker extends LetterRemainderLookup {
+  val checkCharIndex = 1
+  val weights = List(0, 0, 9, 10, 11, 12, 13, 8, 7, 6, 5, 4, 3, 2, 1)
+  val letterToNumber = Map('A' -> 33, 'B' -> 34, 'C' -> 35, 'D' -> 36, 'E' -> 37, 'F' -> 38, 'G' -> 39, 'H' -> 40, 'I' -> 41, 'J' -> 42, 'K' -> 43, 'L' -> 44, 'M' -> 45,
+    'N' -> 46, 'O' -> 47, 'P' -> 48, 'Q' -> 49, 'R' -> 50, 'S' -> 51, 'T' -> 52, 'U' -> 53, 'V' -> 54, 'W' -> 55, 'X' -> 56, 'Y' -> 57, 'Z' -> 58)
+  val refRegex = """^X[A-Z](([0-9A-Z]{13})|([0-9A-Z]\d{11}))$"""
+}
+
+object SdltReferenceChecker extends LetterRemainderLookup {
+  val checkCharIndex = 10
+  val weights = List(6, 7, 8, 9, 10, 5, 4, 3, 2)
+  val letterToNumber  =
+    Map('A' -> 0, 'B' -> 1, 'C' -> 2, 'D' -> 3, 'E' -> 4, 'F' -> 5, 'G' -> 6, 'H' -> 7, 'X' -> 8, 'J' -> 9, 'K' -> 10,
+      'L' -> 11, 'M' -> 12, 'N' -> 13, 'Y' -> 14, 'P' -> 15, 'Q' -> 16, 'R' -> 17, 'S' -> 18, 'T' -> 19, 'Z' -> 20, 'V' -> 21, 'W' -> 22)
+  val refRegex = """^\d{9}M[A-Z]$"""
+}
---
+++ src/test/scala/uk/gov/hmrc/referencechecker/ModulusCheckerSpec.scala
@@ -0,0 +1,210 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.referencechecker
+
+import org.scalacheck.Gen
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{Matchers, WordSpecLike}
+
+class ModulusCheckerSpec extends WordSpecLike with Matchers with GeneratorDrivenPropertyChecks {
+
+  "The SA reference checker" should {
+
+    val ValidReferences = Seq("2234567890K", "2108834503K", "1097172564K", "2234567890", "K2234567890", "1097172564k", "k1097172564")
+    val InvalidReferences = Seq("", "123456", "1234567890K", "inv@lid123K", "inv@lid123K", "1nv@l-d~&*!", "1234567890E", "12345678905", "1097172564KZ")
+
+    "return true for a valid reference" in {
+      checkAllValid(SelfAssessmentReferenceChecker, ValidReferences)
+    }
+
+    "return false for an invalid reference" in {
+      checkAllInvalid(SelfAssessmentReferenceChecker, InvalidReferences)
+    }
+
+    "return true for correctly generated references" in {
+      val checkSum = Gen.listOfN(9, Gen.chooseNum(0, 9))
+
+      val weights = List(6, 7, 8, 9, 10, 5, 4, 3, 2)
+      val remainderLookupTable = List(2,1,9,8,7,6,5,4,3,2,1)
+
+      forAll(checkSum) { (ints: Seq[Int]) =>
+        val remainder = (ints, weights).zipped.map {
+          case (s, i) => s * i
+        }.sum % 11
+        val firstNumber: Int = remainderLookupTable(remainder)
+        SelfAssessmentReferenceChecker.isValid(firstNumber + ints.mkString + "K") shouldBe true
+      }
+    }
+
+  }
+
+  "CT checksum validator" should {
+    val ValidReferences = Seq("1097172564", "2108834503", "2234567890")
+    val InvalidReferences = Seq("", "123456", "5550000621", "inv@lid123")
+
+
+    "return true for a valid reference" in {
+      checkAllValid(CorporationTaxReferenceChecker, ValidReferences)
+    }
+
+    "return false for an invalid reference" in {
+      checkAllInvalid(CorporationTaxReferenceChecker, InvalidReferences)
+    }
+
+
+    "return true for correctly generated references" in {
+      val checkSum = Gen.listOfN(9, Gen.chooseNum(0, 9))
+
+      val weights = List(6, 7, 8, 9, 10, 5, 4, 3, 2)
+      val remainderLookupTable = List(2, 1, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+
+      forAll(checkSum) { (ints: Seq[Int]) =>
+        val remainder = (ints, weights).zipped.map {
+          case (s, i) => s * i
+        }.sum % 11
+        val firstNumber: Int = remainderLookupTable(remainder)
+        CorporationTaxReferenceChecker.isValid(firstNumber + ints.mkString) should be (true)
+      }
+    }
+
+  }
+
+  "The VAT reference checker" should {
+
+    val ValidReferences = Seq("101747696", "101747641", "000000097")
+    val InvalidReferences = Seq("", "123456", "123456789", "123abc789")
+
+    "return true for a valid reference" in {
+      checkAllValid(VatReferenceChecker, ValidReferences)
+    }
+
+    "return false for an invalid reference" in {
+      checkAllInvalid(VatReferenceChecker, InvalidReferences)
+    }
+
+    val vatWeights = List(8, 7, 6, 5, 4, 3, 2)
+
+    "return true for correctly generated references for modulo 97" in {
+      val checkSum = Gen.listOfN(7, Gen.chooseNum(0, 9))
+
+      forAll(checkSum) { (ints: Seq[Int]) =>
+        whenever(ints.size == 7) {
+          var sums = (ints, vatWeights).zipped.map {
+            case (s, i) => s * i
+          }.sum
+
+          while (sums > 97) sums -= 97
+
+          val reference = ints.mkString + "%02d".format(97 - sums)
+          VatReferenceChecker.isValid(reference) shouldBe true
+        }
+      }
+    }
+
+    "return true for correctly generated references for modulo 97 55" in {
+      val checkSum = Gen.listOfN(7, Gen.chooseNum(0, 9))
+
+      forAll(checkSum) { (ints: Seq[Int]) =>
+        whenever(ints.size == 7) {
+          var sums = (ints, vatWeights).zipped.map {
+            case (s, i) => s * i
+          }.sum + 55
+
+          while (sums > 97) sums -= 97
+
+          val reference = ints.mkString + "%02d".format(97 - sums)
+          VatReferenceChecker.isValid(reference) shouldBe true
+        }
+      }
+    }
+  }
+
+  "The ePAYE reference checker" should {
+
+    val ValidReferences = Seq("123PW12345678", "450PE00923437", "120PY19064375", "450PA0096437K", "853PW0076584X", "846PS00928342", "961PA0045782X", "961PF10305739", "123PH45678900")
+    val InvalidReferences = Seq("", "1234", "000PL00000000", "120PA12697495", "846PT00568772", "820PB0091254X", "961PI004578OQ", "123PW12345678Z", "8465L04697138", "120PW003179212324", "120PW0024", "123XH4!@£900", "961PA1145782X", "961PL004578FQ")
+
+    "return true for a valid reference" in {
+      checkAllValid(EpayeReferenceChecker, ValidReferences)
+    }
+
+    "return false for an invalid reference" in {
+      checkAllInvalid(EpayeReferenceChecker, InvalidReferences)
+    }
+  }
+
+  "The Other taxes reference checker" should {
+
+    val Valid14DigitReferences = Seq("XA012345678901", "XDS98712398712", "XDS98712398712", "XAP74589243785", "XE123456789012", "XM123995577846", "XM003425669813")
+    val Valid15DigitReferences = Seq("XP00CATINTHEHAT", "XH1234567890123", "XQ0938588127545", "XK0046537877231", "XRgreeneggs4ham", "XM0yertleturtle")
+
+    val Invalid14DigitReferences = Seq("XF093624679222", "XW135792468123", "XFG03589343785", "inv@lid12A3456", "12345678901234", "X1234567890123", "XA1234567890AA")
+    val Invalid15DigitReferences = Seq("X12345678901233", "XJ9876543210123", "XAP74589243785Z", "XP1234567890120", "XM7846942270421", "X23456789012345")
+    val InvalidReferences = Seq("", "123456", "123456789A123")
+
+    "return true for a valid 14 digit reference" in {
+      checkAllValid(OtherTaxReferenceChecker, Valid14DigitReferences)
+    }
+
+    "return true for a valid 15 digit reference" in {
+      checkAllValid(OtherTaxReferenceChecker, Valid15DigitReferences)
+    }
+
+    "return false for an invalid 14 digit reference" in {
+      checkAllInvalid(OtherTaxReferenceChecker, Invalid14DigitReferences)
+    }
+
+    "return false for an invalid 15 digit reference" in {
+      checkAllInvalid(OtherTaxReferenceChecker, Invalid15DigitReferences)
+    }
+
+    "return false for an reference" in {
+      checkAllInvalid(OtherTaxReferenceChecker, InvalidReferences)
+    }
+  }
+
+  "The SDLT reference checker" should {
+
+    val ValidReferences = Seq("123456789MA", "918273645MV", "576682142MP", "586110842MR", "143245939MZ", "345726781MA","335626481MS", "680686481MW","430622221MH","222222222MQ", "123456789MA")
+    val InvalidReferences = Seq("", "12345", "233795664MB", "946725624MQ", "848785828MB","151617181MC","273647567MJ", "632926282MQ","246485202MQ","426442202MQ","486848282MA","222222222MX", "A23456789MA", "1A3456789MA", "12A456789MA", "123A56789MA", "1234A6789MA", "12345A789MA", "123456A89MA", "1234567A9MA", "12345678AMA", "576682142MPZ", "123456789MAAAAAAA", "123456789DA", "123456789M2")
+
+    "return true for a valid reference" in {
+      checkAllValid(SdltReferenceChecker, ValidReferences)
+    }
+
+    "return false for an invalid reference" in {
+      checkAllInvalid(SdltReferenceChecker, InvalidReferences)
+    }
+
+  }
+
+  def checkAllValid(checker: ReferenceChecker, refs: Seq[String]) = {
+    val invalidRefs = refs.filterNot(checker.isValid)
+
+    withClue("Invalid references found: " + invalidRefs + "\n") {
+      invalidRefs should be (empty)
+    }
+  }
+
+  def checkAllInvalid(checker: ReferenceChecker, refs: Seq[String]) = {
+    val validRefs = refs.filter(checker.isValid)
+
+    withClue("Valid references found: " + validRefs + "\n") {
+      validRefs should be (empty)
+    }
+  }
+}
